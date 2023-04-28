a8f4fb1da4979fcdefbea3ff21a0fe9233047a30
ahbeym <101992409+ahbeym@users.noreply.github.com>
2023-04-28 17:37:02 -0400

    t

--- src/main/java/com/example/coursereviewhw7/Data.java
+++ src/main/java/com/example/coursereviewhw7/Data.java
@@ -1,4 +1,16 @@
 package com.example.coursereviewhw7;
+import java.sql.*;
+//When your program begins, it should check if "Reviews.sqlite3" exists.
+// If the database file doesn't exist:
+// Your program must create the Reviews.sqlite3 database file in the project root directory
+// Run the necessary "CREATE TABLE" queries to put the three required tables into the database
+// If the database file does exist, you don't need to do anything special.
 
+//remember to cite the reused code
 public class Data {
+   void testing() throws SQLException {
+      Connection hi;
+      hi = DriverManager.getConnection("jdbc:sqlite:Reviews");
+
+   }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.coursereviewhw7 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.coursereviewhw7 to javafx.fxml;
