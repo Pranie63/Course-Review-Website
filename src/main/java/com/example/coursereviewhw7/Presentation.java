05e415933abaca545818a0536c21848e82e39ff3
arnavsingh <sgz6xd@virginia.edu>
2023-04-29 17:49:21 -0400

    FXML file testing

--- src/main/java/com/example/coursereviewhw7/Presentation.java
+++ src/main/java/com/example/coursereviewhw7/Presentation.java
@@ -1,4 +1,22 @@
 package com.example.coursereviewhw7;
+import java.util.Scanner;
+import java.sql.*;
+import java.sql.Connection;
 
 public class Presentation {
+    
+    Connection c;
+
+
+    Statement s;
+
+    {
+        try {
+            s = c.createStatement();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
