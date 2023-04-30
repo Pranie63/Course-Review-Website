af6eec75f259300b1be2a652a5395bfd2824cf13
ahbeym <101992409+ahbeym@users.noreply.github.com>
2023-04-30 01:10:13 -0400

    set up Review, Course, and Student classes with appropriate constructors, getters, and setters.

--- src/main/java/com/example/coursereviewhw7/Review.java
+++ src/main/java/com/example/coursereviewhw7/Review.java
@@ -1,4 +1,25 @@
 package com.example.coursereviewhw7;
 
 public class Review {
+    private int rating;
+    private String reviewText;
+
+
+    public Review(int rating, String reviewText){
+        this.rating = rating;
+        this.reviewText = reviewText;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+    public void setRating(int rating){
+        this.rating = rating;
+    }
+    public String getReviewText() {
+        return reviewText;
+    }
+    public void setReviewText(String reviewText){
+        this.reviewText = reviewText;
+    }
 }
