1a7a4b6b9be4cd93526da5239f7c7d20274e2f27
arnavsingh <sgz6xd@virginia.edu>
2023-05-03 22:27:09 -0400

    Changing the design of the login page

--- src/main/java/com/example/coursereviewhw7/LoginApplication.java
+++ src/main/java/com/example/coursereviewhw7/LoginApplication.java
@@ -12,7 +12,6 @@ public class LoginApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
